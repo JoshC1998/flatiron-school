87b2d65c2f8c292fd4dd282439d88f3c47fdfe34
Joshua Cruz <jcruz1998@gmail.com>
2024-06-06 11:52:00 -0400

    done 6/6

--- phase-1/fundamentals/introduction/end/index.js
+++ phase-1/fundamentals/introduction/end/index.js
@@ -8,97 +8,7 @@ Created August 21, 2023
 */
 
 /*
-
-Welcome to **JavaScript**, a language built to add interactivity to webpages!
-
-Like most programming languages, JavaScript employs:
-- **variables** to name **values** of different **types**,
-- **objects** and **arrays** to consolidate related values,
-- **functions** to store and execute sets of instructions,
-- **conditional statements** to carry out instructions on a conditional basis,
-- **scope** to decide where variables and functions can be invoked, and
-- **methods** to carry out tasks associated with a variable of a certain type.
-
-Like HTML, JS also lets you write comments, like this one! Multiline comments
-are surrounded by [slash][star] and [star][slash], while single line comments
-are simply preceded by [slash][slash].
-
-Let's dive in!
-
-*/
-
-// ~ Variables
-console.log("~~~"); // This line prints "~~~" to the console.
-console.log("VARIABLES"); // This line prints "VARIABLES" to the console.
-// In JavaScript, you can give values (e.g. 5, "hi") names.
-// We refer to those names as **variables**.
-// To **declare** a variable, prepend its name with either `let` or `const`.
-// To **assign** a value to a variable, write [variable name] = [value].
-// To do both on the same line, write [let | const] [variable] = [value].
-let x = 0;
-console.log("x: " + x); // This line prints "x: [x]" to the console.
-// A variable declared with the keyword `let` can be re-assigned.
-console.log("Mutating `x`..."); // **Mutation** is a synonym for reassigment.
-x = 1; // This is OK. `x` was declared with the keyword `let`.
-console.log("x: " + x); // This line prints "x: [x]" to the console.
-// A variable assigned with the keyword `const` cannot be re-assigned.
-// Note that you can't _only_ declare with `const`.
-// A constant variable must be declared and assigned on the same line.
-// const y; // This is NOT OK, because we aren't stating y's constant value.
-const y = 1;
-// console.log("Mutating `y`..."); // This and the following line are NOT OK,
-// y = 2; // because `y` was declared with the keyword `const`.
-console.log("y: " + y);
-// It's best practice to declare variables with `const`, unless you're sure
-//      you need the variable to be **mutable**, i.e., able to be re-assigned.
-// This way, you'll avoid making unwanted changes to values.
-// We can remind ourselves and others what our variables represent with **annotations**.
-/** The number of apples we have in stock. */
-const apples = 5;
-
-// ~ Types
-console.log("~~~");
-console.log("TYPES");
-// There are seven **primitive** (basic) types of values you can assign to variables in JavaScript:
-//       undefined, null, boolean, number, string, bigint, and symbol.
-// You can use `console.log(typeof(var))` to print the type of `var` to console.
-// -> **undefined** and **null** variables have no (useful) value.
-// **undefined** is a type automatically assigned to unassigned variables.
-// **null** is a type you can manually assign to intentionally skip assigning anything useful.
-// Note: Due to an unrepairable bug in JavaScript, `typeof(var)` returns `object` when `var` is `null`!
-let b; console.log("b is " + typeof(b) + ""); // undefined
-let c = null; console.log("c is a " + c + " " + typeof(c)); // null
-// -> A **boolean** can be true or false.
-const fact = true; console.log(fact + " is a " + typeof(fact)); // boolean
-const lie = false; console.log(lie + " is a " + typeof(lie)); // boolean
-// -> A **number** can be any rational number between -2^53 + 1 and 2^53 - 1.
-const naturalNumber = 3; // To name a variable with more than one word, use camelCase.
-console.log(naturalNumber + " is a " + typeof(naturalNumber)); // number
-const integer = -2; console.log(integer + " is a " + typeof(integer)); // number
-const decimal = 2.43; console.log(decimal + " is a " + typeof(decimal)); // number
-// -> A **string** is one or more characters surrounded by 's, "s, or `s.
-// Strings can be **concatenated**, e.g. "Hello" + "!", to form longer strings.
-// They can also **interpolate** variables, e.g. `Hello, ${firstName}!`, to incorporate outside values.
-const greeting = "Hello, world!"; console.log(`${greeting} is a ${typeof(greeting)}`); // string
-// -> **bigint** is meant for numbers too negatively or positively large to be allowed by **number**.
-const tinyNumber = BigInt("-9007199254740992"); // -9007199254740992 is one less than -2^53 + 1.
-console.log(tinyNumber + " is a " + typeof(tinyNumber)); // bigint
-const hugeNumber = BigInt("9007199254740992"); // 9007199254740992 is one more than 2^53 - 1.
-console.log(hugeNumber + " is a " + typeof(hugeNumber)); // bigint
-// -> A **symbol** is a unique value.
-const symbol = Symbol(); console.log(symbol.toString() + " is a " + typeof(symbol));
-
-// ~ Objects
-console.log("~~~");
-console.log("OBJECTS");
-// An **object** is a collection of related **properties**, or **keys**, each with their own value.
-// They are denoted with surrounding { curly braces } and comma-separated key: value pairs.
-// For example, when setting out to define a recipe, we might realize that a single value of a single type
-// does not suffice. To create a recipe consisting of many related values, we create an object!
-const recipe = {
-    name: "tacos", // Here, the value "Tacos" is bound to the key "name".
-    "author": "Sakib Rasul", // Keys can be surrounded by "s...
-    'easy': true, // ...or 's, but not `s!
+e, // ...or 's, but not `s!
     servings: 4 // Values can be of any type.
 };
 console.log(recipe); // We can print an object's properties with `console.log()`.
@@ -272,5 +182,95 @@ function sum(a, b) { console.log(a + b) }; // Declaration
 // CHALLENGES
 // Try these practice problems on your own to reinforce this lesson's material :)
 // 1. Write a function named `sum` that takes an array of `numbers` and returns its sum.
-// 2. Write a function named `double` that takes an array of `numbers` and doubles each of its values.
+// 2. Write a f
+Welcome to **JavaScript**, a language built to add interactivity to webpages!
+
+Like most programming languages, JavaScript employs:
+- **variables** to name **values** of different **types**,
+- **objects** and **arrays** to consolidate related values,
+- **functions** to store and execute sets of instructions,
+- **conditional statements** to carry out instructions on a conditional basis,
+- **scope** to decide where variables and functions can be invoked, and
+- **methods** to carry out tasks associated with a variable of a certain type.
+
+Like HTML, JS also lets you write comments, like this one! Multiline comments
+are surrounded by [slash][star] and [star][slash], while single line comments
+are simply preceded by [slash][slash].
+
+Let's dive in!
+
+*/
+
+// ~ Variables
+console.log("~~~"); // This line prints "~~~" to the console.
+console.log("VARIABLES"); // This line prints "VARIABLES" to the console.
+// In JavaScript, you can give values (e.g. 5, "hi") names.
+// We refer to those names as **variables**.
+// To **declare** a variable, prepend its name with either `let` or `const`.
+// To **assign** a value to a variable, write [variable name] = [value].
+// To do both on the same line, write [let | const] [variable] = [value].
+let x = 0;
+console.log("x: " + x); // This line prints "x: [x]" to the console.
+// A variable declared with the keyword `let` can be re-assigned.
+console.log("Mutating `x`..."); // **Mutation** is a synonym for reassigment.
+x = 1; // This is OK. `x` was declared with the keyword `let`.
+console.log("x: " + x); // This line prints "x: [x]" to the console.
+// A variable assigned with the keyword `const` cannot be re-assigned.
+// Note that you can't _only_ declare with `const`.
+// A constant variable must be declared and assigned on the same line.
+// const y; // This is NOT OK, because we aren't stating y's constant value.
+const y = 1;
+// console.log("Mutating `y`..."); // This and the following line are NOT OK,
+// y = 2; // because `y` was declared with the keyword `const`.
+console.log("y: " + y);
+// It's best practice to declare variables with `const`, unless you're sure
+//      you need the variable to be **mutable**, i.e., able to be re-assigned.
+// This way, you'll avoid making unwanted changes to values.
+// We can remind ourselves and others what our variables represent with **annotations**.
+/** The number of apples we have in stock. */
+const apples = 5;
+
+// ~ Types
+console.log("~~~");
+console.log("TYPES");
+// There are seven **primitive** (basic) types of values you can assign to variables in JavaScript:
+//       undefined, null, boolean, number, string, bigint, and symbol.
+// You can use `console.log(typeof(var))` to print the type of `var` to console.
+// -> **undefined** and **null** variables have no (useful) value.
+// **undefined** is a type automatically assigned to unassigned variables.
+// **null** is a type you can manually assign to intentionally skip assigning anything useful.
+// Note: Due to an unrepairable bug in JavaScript, `typeof(var)` returns `object` when `var` is `null`!
+let b; console.log("b is " + typeof(b) + ""); // undefined
+let c = null; console.log("c is a " + c + " " + typeof(c)); // null
+// -> A **boolean** can be true or false.
+const fact = true; console.log(fact + " is a " + typeof(fact)); // boolean
+const lie = false; console.log(lie + " is a " + typeof(lie)); // boolean
+// -> A **number** can be any rational number between -2^53 + 1 and 2^53 - 1.
+const naturalNumber = 3; // To name a variable with more than one word, use camelCase.
+console.log(naturalNumber + " is a " + typeof(naturalNumber)); // number
+const integer = -2; console.log(integer + " is a " + typeof(integer)); // number
+const decimal = 2.43; console.log(decimal + " is a " + typeof(decimal)); // number
+// -> A **string** is one or more characters surrounded by 's, "s, or `s.
+// Strings can be **concatenated**, e.g. "Hello" + "!", to form longer strings.
+// They can also **interpolate** variables, e.g. `Hello, ${firstName}!`, to incorporate outside values.
+const greeting = "Hello, world!"; console.log(`${greeting} is a ${typeof(greeting)}`); // string
+// -> **bigint** is meant for numbers too negatively or positively large to be allowed by **number**.
+const tinyNumber = BigInt("-9007199254740992"); // -9007199254740992 is one less than -2^53 + 1.
+console.log(tinyNumber + " is a " + typeof(tinyNumber)); // bigint
+const hugeNumber = BigInt("9007199254740992"); // 9007199254740992 is one more than 2^53 - 1.
+console.log(hugeNumber + " is a " + typeof(hugeNumber)); // bigint
+// -> A **symbol** is a unique value.
+const symbol = Symbol(); console.log(symbol.toString() + " is a " + typeof(symbol));
+
+// ~ Objects
+console.log("~~~");
+console.log("OBJECTS");
+// An **object** is a collection of related **properties**, or **keys**, each with their own value.
+// They are denoted with surrounding { curly braces } and comma-separated key: value pairs.
+// For example, when setting out to define a recipe, we might realize that a single value of a single type
+// does not suffice. To create a recipe consisting of many related values, we create an object!
+const recipe = {
+    name: "tacos", // Here, the value "Tacos" is bound to the key "name".
+    "author": "Sakib Rasul", // Keys can be surrounded by "s...
+    'easy': truunction named `double` that takes an array of `numbers` and doubles each of its values.
 // 3. Write a function named `lowercase` that takes an array of `words` and returns a lowercased copy.
--- phase-1/manipulation/beginning/index.js
+++ phase-1/manipulation/beginning/index.js
@@ -18,7 +18,10 @@ Challenges
 */
 
 // ~ APIs, CRUD, `document`
-
+//document is an API that lets us interact (CRUD) with HTML!
+console.log(document.querySelector("hi"));
+console.log(document.querySelector("#forecer").id)
+ //has to be a string
 // ~ Read/Select a node
 // -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
 // -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
@@ -27,20 +30,45 @@ Challenges
 
 // ~ Update/Modify a node's attributes
 // -> To modify an attribute, just use = after the attribute name in object dot notation
-
+document.querySelector("#forever").textContent = "Forever!";
 // ~ Delete/Remove a node
 // -> To remove an existing element, we can look it up and call the node's method `remove()`.
+document.querySelector("#forever").remove();
 
 // ~ Create + Append a node
 // -> createElement(), append()
-
+// 1. Create an element.
+const pElement = document.createElement("p");
+// 2. Modify the new element 
+pElement.textContent = "On June 4th,2024";
+// 3. Append the new element to an existing one 
+document.querySelector("div").append(pElement);
 // ~ Challenges
 // 1. Write a function named displayList that takes a name and an array,
 //    and appends a list to #dates. For example, given "Books" and "The Shining",
-//    the function should append to #dates something like:
+//    the function should a ppend to #dates something like:
 //        Books
 //        • The Shining
 // 2. Replace the <strong> element with a newly created one.
 
+movies = ["Avengers", "Up", "Wallie"]
+const displayList = {name, movies} =>{
+    const h2Element = document.createElement("h2");
+    h2Element.textcontext = name
+    document.querySelector("div").append(h2Element)
+    const ulList = document.createElement("ul");
+    document.querySelector("div").append(ulList)
 
+    movies.forEach((movie)=> {
+        const liList = document.createElement("li");
+        liList.textContext = movie
+        document.querySelector("ul").append(liList)
+    })
+}
 
+displayList("Movies",movies)
+
+const strong = document.querySelector("strong");
+const italic = document.createElement("em");
+italic.textContext = "Pedro Vicenty"
+strong.replaceWith(italic)
